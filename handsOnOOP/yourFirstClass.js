9ca5eda4de8b207dbbee9b39bcf1eb89c6aa93fa
Abduraimov Muminjon <muminjonguru@outlook.com>
2021-01-18 20:57:32 +0500

    ECMA2015 based class

---
+++ handsOnOOP/yourFirstClass.js
@@ -0,0 +1,12 @@
+class Pet {
+  constructor(animal, age, breed) {
+    this.animal = animal;
+    this.age = age;
+    this.breed = breed;
+  }
+}
+
+
+const oggy = new Pet('cat', 1, 'jug');
+
+console.log(oggy);
